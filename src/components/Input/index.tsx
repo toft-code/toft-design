801593b42520a7da45b16787c3ed5ef436363ed7
limichange <limichange@hotmail.com>
2020-10-23 18:28:05 +0800

    Update

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss'
+
+type Props = {}
+
+const Input: React.FC<Props> = (props) => {
+  return <input className='input' type='text' />
+}
+
+export default Input
