545fdd7afe27951a458115f09eb65d89bc5c7d84
limichange <limichange@hotmail.com>
2020-10-24 16:09:49 +0800

    update: button input style

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -6,13 +6,40 @@ import './index.css'
 function App() {
   return (
     <div className='App bodyContainer'>
+      {/* Button */}
       <h1>Button</h1>
-      <Button primary>Button</Button>
-      <div style={{ marginTop: 10 }}></div>
-      <Button>Button</Button>
 
+      <div>
+        <span style={{ margin: 8 }}>
+          <Button primary size='small'>
+            Button
+          </Button>
+        </span>
+        <span style={{ margin: 8 }}>
+          <Button primary>Button</Button>
+        </span>
+        <span style={{ margin: 8 }}>
+          <Button primary size='large'>
+            Button
+          </Button>
+        </span>
+
+        <span style={{ margin: 8 }}>
+          <Button size='small'>Button</Button>
+        </span>
+        <span style={{ margin: 8 }}>
+          <Button>Button</Button>
+        </span>
+        <span style={{ margin: 8 }}>
+          <Button size='large'>Button</Button>
+        </span>
+      </div>
+
+      {/* Input */}
       <h1>Input</h1>
       <Input placeholder='Please Input'></Input>
+
+      <h1>Tag</h1>
     </div>
   )
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,15 +4,24 @@ import './index.scss'
 type Props = {
   onClick?: Function
   primary?: boolean
+  size?: 'large' | 'normal' | 'small'
 }
 
 const Button: React.FC<Props> = (props) => {
-  const { children, primary } = props
+  const { children, primary, size } = props
 
   let className = 'button'
 
   if (primary) {
     className += ' primary'
+  }
+
+  if (size === 'large') {
+    className += ' large'
+  }
+
+  if (size === 'small') {
+    className += ' small'
   }
 
   return <button className={className}>{children}</button>
